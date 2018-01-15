53d315c1776a4d7c702396dea1206c10eb68dcb0
Mohd Adib <relesafe@gmail.com>
2018-01-15 15:36:23 +0800

    update

--- all.sh
+++ all.sh
@@ -8,7 +8,7 @@ service sshd restart
 adduser deployer && gpasswd -a deployer sudo
 su deployer
 
-export LANGUAGE=en_US.UTF-8 && export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && locale-gen en_US.UTF-8 && sudo dpkg-reconfigure locales
+export LANGUAGE=en_US.UTF-8 && export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && sudo locale-gen en_US.UTF-8 && sudo dpkg-reconfigure locales
 
 sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
